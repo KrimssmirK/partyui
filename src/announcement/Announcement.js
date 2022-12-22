7883b0e5971f67613afb7d2ab6876b31f6b3a3cd
KrimssmirK <krimssmirk003@gmail.com>
2022-12-22 17:06:50 +0800

    added fetch function

--- src/announcement/Announcement.js
+++ src/announcement/Announcement.js
@@ -1,35 +1,69 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 
+// React-Bootstrap
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+// styling with css
 import './Announcement.css';
+
+// image components
 import foodImg from './img/food.png';
 
+const initialAnnouncements = [
+  {img: foodImg, date: '2022.1.5', title: 'Halloween 15% Discount Promo', description: 'BBQ & Drinks Plan Adult・senior high: ￥3,500 to ￥ 2,975...'},
+];
+
+const announcementReducer = (currentState, action) => {
+  switch (action.type) {
+    case 'ANNOUNCEMENT_FETCH_SUCCESS':
+      return {
+        data: action.payload,
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const getAsyncData = () =>
+  new Promise(resolve => {
+    setTimeout(() => resolve({ data: { announcements: initialAnnouncements }})
+    , 2000);
+  });
+
+const API_ENDPOINT = '';                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
 const Announcement = () => {
-  const announcementItems = [
-    {img: foodImg, date: '2022.1.5', title: 'Halloween 15% Discount Promo', description: 'BBQ & Drinks Plan Adult・senior high: ￥3,500 to ￥ 2,975...'},
-    {img: foodImg, date: '2022.1.5', title: 'Halloween 15% Discount Promo', description: 'BBQ & Drinks Plan Adult・senior high: ￥3,500 to ￥ 2,975...'},
-    {img: foodImg, date: '2022.1.5', title: 'Halloween 15% Discount Promo', description: 'BBQ & Drinks Plan Adult・senior high: ￥3,500 to ￥ 2,975...'},
-    {img: foodImg, date: '2022.1.5', title: 'Halloween 15% Discount Promo', description: 'BBQ & Drinks Plan Adult・senior high: ￥3,500 to ￥ 2,975...'},
-  ];
+  const [announcements, dispatchAnnouncements] = useReducer(announcementReducer, { data: [] });
+  
+  // it is similar to componentDidMount and componentDidUpdate
+  useEffect(() => {
+    getAsyncData()
+      .then(result => dispatchAnnouncements({
+        type: 'ANNOUNCEMENT_FETCH_SUCCESS',
+        payload: result.data.announcements,
+      }))
+      .catch(() => new Error());
+  }, [announcements]
+  );
+
   return (
     <div className='Announcement'>
     <div className='Announcement-title'>
       <h1>ANNOUNCEMENT</h1>
     </div>
     <div className='Announcement-list'>
-      <AnnouncementItem img={foodImg} date={'2022.1.5'} title={'I love you'} description={'BBQ & Drinks Plan Adult・senior high: ￥3,500 to ￥ 2,975...'} />
       {
-        announcementItems.map(item =>
-          <AnnouncementItem {...item} />
-        )
+        announcements.data.map(item => <AnnouncementItem {...item} />)
       }
     </div>
   </div>
   );
-}
+};
+  
+
+
   
 
 const AnnouncementItem = ({ img, date, title, description }) =>
